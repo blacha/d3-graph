51c2bd57d63bcfa93b978f3bd15710241635f393
Blayne Chard <blayne@chard.co.nz>
2012-04-15 21:56:12 +1200

    convert to using a nicer context layout with sub contexts for the key and arc configs

--- arcgraph.js
+++ arcgraph.js
@@ -23,33 +23,35 @@
 
 
 ArcGraph = function(ctx){
-    if (ctx.width === undefined){
-        throw Error('No width given.');
-    }
+    if (ctx.width === undefined){ throw Error('No width given.'); }
     this.w = ctx.width;
 
-    if (ctx.height === undefined){
-        throw Error('No height given.');
-    }
+    if (ctx.height === undefined){ throw Error('No height given.'); }
     this.h = ctx.height;
 
-    if (ctx.node === undefined){
-        throw Error('No Node given');
-    }
+    if (ctx.node === undefined){ throw Error('No Node given'); }
     this.node =  ctx.node;
 
+    // Raidus of the circle
     this.r = Math.min(this.w, this.h) / 2;
 
-    this.arc_width = ctx.arc_width || 20;
-    this.arc_max_angle = ctx.arc_angle || 1.66;
-    this.description_height = ctx.description_height || 50;
-    this.internal_whitespace = ctx.internal_whitespace === undefined ? 1 - 0.25 : 1 - ctx.internal_whitespace;
+    // Arc config
+    this.arc = ctx.arc || {};
+    this.arc.width = this.arc.width || 20;
+    this.arc.end_angle = this.deg_to_rad(this.arc.end_angle) || 1.66 * Math.PI; // end 3/4 around
+    this.arc.start_angle = this.deg_to_rad(this.arc.start_angle) || Math.PI; // Start at the bottom
+    this.arc.internal = this.arc.internal || 0.25; // amount of space to be left as white space
+    this.arc.offset_x = this.arc.offset_x || 5; // x offset of the graph
+    this.arc.offset_y = this.arc.offset_y || 0; // y offset of the graph
 
-    this.get_color = ctx.get_colors === undefined ? this.get_color : ctx.get_colors;
-    this.colors = ctx.colors || [ '#0093d5', '#f1b821', '#009983','#cf3d96', '#df7627', '#252', '#528', '#72f', '#444'];
+    // Key config
+    this.key = ctx.key || {};
+    this.key.height = this.key.height || 50;
+    this.key.height_percent = this.key.height_percent || 0.8; // Use only 80% of the height for the key
+    this.key.line_color = this.key.line_color || '#888';
+    this.key.text_color = this.key.text_color || '#666';
 
-    this.line_color = ctx.line_color || '#888';
-    this.description_color = ctx.description_color || '#666';
+    this.colors = ctx.colors || ['#0093d5', '#f1b821', '#009983','#cf3d96', '#df7627', '#252', '#528', '#72f', '#444'];
 
     if (ctx.data !== undefined && ctx.data !== null){
         this.update_data(ctx.data);
@@ -57,35 +59,26 @@ ArcGraph = function(ctx){
 };
 
 ArcGraph.prototype = {
-    get_color: function(index){
-        return this.colors[index];
-    },
 
     update_data: function(data){
-        if (this.get_color === undefined){
-            this.get_color = function(i){
-                return this.colors[i];
-            };
-        }
-
         if (data.length === undefined || data.length === 0) { return; }
         this.data = data.sort(this._compare_node);
         var max = this.data[0].value;
 
         // Check to see if the arc width is too big, if it is reduce the width of the arcs
-        if (this.data.length * this.arc_width > this.r * this.internal_whitespace){
-            this.arc_width = this.r * this.internal_whitespace / this.data.length;
+        if (this.data.length * this.arc.width > this.r * (1 - this.arc.internal)){
+            this.arc.width = this.r * (1 - this.arc.internal) / this.data.length;
         }
 
         // Check to see if the description is too big, if it is reduce the height of the descripion
-        if (this.data.length * this.description_height > this.h * 0.8){
-            this.description_height = this.h / this.data.length * 0.8;
+        if (this.data.length * this.key.height > this.h * this.key.height_percent){
+            this.key.height = this.h / this.data.length * this.key.height_percent;
         }
 
         var me = this;
         var arc = d3.svg.arc()
-             .startAngle(function(d, i) { return Math.PI; })
-             .endAngle(function(d, i) { return d.value / max * Math.PI * me.arc_max_angle + Math.PI; })
+             .startAngle(function(d, i) { return me.arc.start_angle; })
+             .endAngle(function(d, i) { return d.value / max  * me.arc.end_angle + me.arc.start_angle; })
              .innerRadius(function(d, i) { return me.innerRadius(i); })
              .outerRadius(function(d, i) { return me.outerRadius(i); });
 
@@ -95,7 +88,7 @@ ArcGraph.prototype = {
             .attr("width", this.w)
             .attr("height", this.h)
             .append('g')
-            .attr("transform", "translate(" + (this.r + 5) + "," + this.r + ")");
+            .attr("transform", "translate(" + (this.r + me.arc.offset_x) + "," + (this.r + me.arc.offset_y) + ")");
 
         // Add the arcs
         var paths = this.vis.selectAll('path')
@@ -107,32 +100,32 @@ ArcGraph.prototype = {
                     return me.node.substring(1, me.node.length) + '-arc-' + i;
                 return d.id;
             })
-            .attr('class', 'ag-arc')
-            .style('fill', function(d, i) {return me.get_color(i); });
+            .attr('class', function(d, i) { return 'ag-arc ag-color-' + i; })
+            .style('fill', function(d, i) { return me.colors[i]; });
 
         // Add the values next to the arcs
-        var arc_font_size = parseInt(this.arc_width * 0.8, 10);
+        var arc_font_size = parseInt(this.arc.width * 0.8, 10);
         this.vis.selectAll('text').data(this.data)
                 .enter().append('text')
                 .attr('x', 5)
                 .attr('y', function(d,i) { return me.outerRadius(i);  } )
                 .attr('dy',  arc_font_size * -0.2)
                 .attr("text-anchor", "start")
-                .attr('class', 'ag-arc-value')
-                .style("fill",  function(d, i) {return me.get_color(i); })
+                .attr('class',  function(d, i) { return 'ag-arc-value ag-color-' + i; })
+                .style("fill",  function(d, i) { return me.colors[i]; })
                 .style('font-size',  arc_font_size + 'px')
                 .text(function(d, i) { return d.value; });
 
         // Add the heading for the description
-        var description_header_font = parseInt(this.description_height * 0.8, 10);
+        var description_header_font = parseInt(this.key.height * 0.8, 10);
         this.vis.selectAll('g').data(this.data)
                 .enter().append('text')
                 .attr('x', this.r * 1.4)
                 .attr('y', function(d, i){ return me.descriptionLocation(i) - 5; })
-                .style("fill",  function(d, i) {return me.get_color(i); })
+                .style("fill",  function(d, i) { return me.colors[i]; })
                 .style('font-size',  description_header_font + 'px')
                 .style('font-weight', 'bold')
-                .attr('class', 'ag-header-value')
+                .attr('class', function(d, i) { return 'ag-header-value ag-color-' + i; })
                 .text(function(d, i) { return d.value2; });
 
         // Add a line under the text
@@ -143,7 +136,7 @@ ArcGraph.prototype = {
                 .attr('y1', function(d, i){ return me.descriptionLocation(i) - 1; } )
                 .attr('y2', function(d, i){ return me.descriptionLocation(i) - 1; } )
                 .attr('class', 'ag-header-line')
-                .style("stroke", this.line_color);
+                .style("stroke", this.key.line_color);
 
         // Add a line under the text
         var description_font = parseInt(description_header_font * 0.5, 10);
@@ -151,23 +144,22 @@ ArcGraph.prototype = {
             .enter().append('text')
                 .attr('x', this.r * 1.45 + description_header_font * 2 )
                 .attr('y', function(d, i){  return me.descriptionLocation(i) - description_header_font / 3; } )
-
-                .style("fill", this.description_color )
+                .style("fill", this.key.text_color )
                 .style('font-size',  description_font + 'px')
                 .attr('class', 'ag-header-description')
                 .text( function(d){ return d.description; });
     },
 
     descriptionLocation: function(i){
-        return this.r - this.description_height *  (this.data.length - 1  - i);
+        return this.r - this.key.height * (this.data.length - 1  - i);
     },
 
     innerRadius: function(i){
-        return this.r - this.arc_width * i - this.arc_width + 1;
+        return this.r - this.arc.width * i - this.arc.width + 1;
     },
 
     outerRadius: function(i){
-        return this.r - this.arc_width * i;
+        return this.r - this.arc.width * i;
     },
 
     _compare_node: function(a, b){
@@ -178,5 +170,15 @@ ArcGraph.prototype = {
             return 1;
         }
         return 0;
+    },
+
+    deg_to_rad: function(deg){
+        if (deg === undefined || deg === null){
+            return undefined;
+        }
+
+        angle = parseInt(deg,10) * Math.PI / 180;
+        console.log(angle)
+        return angle;
     }
 };
